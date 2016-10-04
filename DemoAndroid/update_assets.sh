f6c7c6965c33aed018dbd47e7916a3352e189862
Balint Koczor <koczor@sg01.4.glaser>
2016-10-04 14:59:42 +0200

    added unix support to android demo

---
+++ DemoAndroid/update_assets.sh
@@ -0,0 +1,10 @@
+mkdir ./assets/language
+mkdir ./assets/font
+mkdir ./assets/skin
+mkdir ./assets/demo_skin
+cp -rf ../resources/language/*.txt assets/language/
+cp -rf ../resources/default_font/* assets/font/
+cp -rf ../resources/default_skin/*.png assets/skin/
+cp -rf ../resources/default_skin/*.txt assets/skin/
+cp -rf ../Demo/demo01/skin/*.png assets/demo_skin/
+cp -rf ../Demo/demo01/skin/*.txt assets/demo_skin/
